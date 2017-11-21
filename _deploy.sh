b2036ae9f8e469d3e876cc9f3d76e049a0da84a1
w201rdada-portfolio-jrosenfeld13-iuxfqx9n <j.rosen.1392@gmail.com>
2017-11-21 03:04:36 +0000

    deploy

--- _deploy.sh
+++ _deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # put your info here and uncomment so git (and GitHub) knows who your commits belong to!
-# git config user.email "calnet@berkeley.edu"
-# git config user.name "Oski Bear"
+git config user.email "j.rosen.1392@gmail.com"
+git config user.name "jrosenfeld13"
 
 # make sure you're on the master branch
 git checkout master
